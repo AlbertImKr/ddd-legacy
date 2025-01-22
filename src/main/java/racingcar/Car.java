77f7b305b011456807f6744bae52b4ca7ccf4e7e
ALbertIM0427 <guang7226961@naver.com>
2025-01-22 18:22:55 +0900

    feat(racingcar): 자동차가 움직이는 조건이 4 이상시 이동하는 기능 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,13 +1,31 @@
 package racingcar;
 
+import java.util.Random;
+
 public class Car {
 
     private final String name;
+    private int position = 0;
 
     public Car(String name) {
         if (name.length() > 5) {
             throw new IllegalArgumentException("자동차 이름은 5글자를 초과할 수 없습니다.");
         }
         this.name = name;
+    }
+
+    public void move(int condition) {
+        if (condition >= 4) {
+            position++;
+        }
+    }
+
+    public void move() {
+        var condition = new Random().nextInt(0, 10);
+        move(condition);
+    }
+
+    public int getPosition() {
+        return position;
     }
 }
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,9 +1,12 @@
 package racingcar;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class CarTest {
 
@@ -17,5 +20,33 @@ class CarTest {
         assertThatThrownBy(() -> new Car(carName))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("자동차 이름은 5글자를 초과할 수 없습니다.");
+    }
+
+    @DisplayName("자동차의 움직이는 조건은 4 이상이면 움직인다.")
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    void if_car_move_condition_is_over_4_then_move(int condition) {
+        // given
+        var car = new Car("car");
+
+        // when
+        car.move(condition);
+
+        // then
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @DisplayName("자동차의 움직이는 조건은 4 미만이면 멈춘다.")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3})
+    void if_car_move_condition_is_under_4_then_stop(int condition) {
+        // given
+        var car = new Car("car");
+
+        // when
+        car.move(condition);
+
+        // then
+        assertThat(car.getPosition()).isZero();
     }
 }
