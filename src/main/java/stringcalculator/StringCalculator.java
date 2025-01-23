60f92f2d085f1ee517670ef526a1094fdb310a03
ALbertIM0427 <guang7226961@naver.com>
2025-01-23 10:40:40 +0900

    feat(stringcalculator): 쉼표(,)와 콜론(:)을 구분자로 숫자를 합한 기능 구현

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -4,12 +4,14 @@ import java.util.Arrays;
 
 public class StringCalculator {
 
+    private static final String DELIMITER = "[,:]";
+
     private StringCalculator() {
         throw new IllegalStateException("Utility class");
     }
 
     public static int add(String input) {
-        return Arrays.stream(input.split(","))
+        return Arrays.stream(input.split(DELIMITER))
                 .mapToInt(Integer::parseInt)
                 .sum();
     }
--- src/test/java/stringcalculator/StringCalculatorTest.java
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -19,4 +19,17 @@ class StringCalculatorTest {
         // then
         assertThat(result).isEqualTo(6);
     }
+
+    @DisplayName("쉼표(,)와 콜론(:)을 구분자로 하는 문자열을 입력할 경우, 쉼표와 콜론을 기준으로 숫자를 분리하여 덧셈을 수행한다.")
+    @Test
+    void if_input_string_contains_comma_and_colon_then_sum_numbers() {
+        // given
+        String input = "1,2:3";
+
+        // when
+        int result = StringCalculator.add(input);
+
+        // then
+        assertThat(result).isEqualTo(6);
+    }
 }
