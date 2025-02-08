ffab8c6f7916edd9aa10e5e2e29014c57a70ad8d
ALbertIM0427 <guang7226961@naver.com>
2025-02-08 21:43:15 +0900

    test(ui): 주문 테이블 목록 조회 테스트 구현

--- src/test/java/kitchenpos/ui/OrderTableRestControllerTest.java
+++ src/test/java/kitchenpos/ui/OrderTableRestControllerTest.java
@@ -3,17 +3,20 @@ package kitchenpos.ui;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.HashMap;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.UUID;
 import kitchenpos.application.OrderTableService;
 import kitchenpos.config.TestConfig;
 import kitchenpos.domain.OrderTable;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -216,6 +219,35 @@ class OrderTableRestControllerTest {
             var response = objectMapper.readValue(result.getResponse().getContentAsString(), OrderTable.class);
             assertThat(response).isNotNull();
             assertThat(response.getId()).isEqualTo(orderTableId);
+        }
+    }
+
+    @DisplayName("주문 테이블 목록 조회")
+    @Nested
+    class ListOrderTables {
+
+        @DisplayName("주문 테이블 목록 조회 성공하면 200 OK를 응답한다.")
+        @Test
+        void if_succeed_then_responds_200_ok() throws Exception {
+            // given
+            var orderTable1 = new OrderTable();
+            orderTable1.setId(UUID.randomUUID());
+            var orderTable2 = new OrderTable();
+            orderTable2.setId(UUID.randomUUID());
+
+            given(orderTableService.findAll()).willReturn(List.of(orderTable1, orderTable2));
+
+            // when
+            var result = mockMvc.perform(get("/api/order-tables"))
+                    // then
+                    .andExpect(status().isOk())
+                    .andReturn();
+
+            var response = objectMapper.readValue(result.getResponse().getContentAsString(), List.class);
+            Assertions.assertAll(
+                    () -> assertThat(response).isNotNull(),
+                    () -> assertThat(response).hasSize(2)
+            );
         }
     }
 }
