8582230a80e6c65de1c632568526061ae2ddc823
ALbertIM0427 <guang7226961@naver.com>
2025-02-09 00:50:29 +0900

    test(ui): 주문 서빙 테스트 구현

--- src/test/java/kitchenpos/ui/OrderRestControllerTest.java
+++ src/test/java/kitchenpos/ui/OrderRestControllerTest.java
@@ -136,4 +136,43 @@ class OrderRestControllerTest {
                     .andExpect(status().isOk());
         }
     }
+
+    @DisplayName("주문 서빙")
+    @Nested
+    class ServeOrder {
+
+        @DisplayName("주문 서빙 실패하면 400 Bad Request를 응답한다.")
+        @Test
+        void if_failed_then_responds_400_bad_request() throws Exception {
+            // given
+            var orderId = UUID.randomUUID();
+            var content = objectMapper.writeValueAsString(new HashMap<>());
+            given(orderService.serve(any())).willThrow(new IllegalArgumentException());
+
+            // when
+            mockMvc.perform(
+                            put("/api/orders/{orderId}/serve", orderId)
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .content(content))
+                    // then
+                    .andExpect(status().isBadRequest());
+        }
+
+        @DisplayName("주문 서빙 성공하면 200 OK를 응답한다.")
+        @Test
+        void if_succeed_then_responds_200_ok() throws Exception {
+            // given
+            var orderId = UUID.randomUUID();
+            var content = objectMapper.writeValueAsString(new HashMap<>());
+            given(orderService.serve(any())).willReturn(FixtureProvider.createFixOrder(orderId, OrderStatus.SERVED));
+
+            // when
+            mockMvc.perform(
+                            put("/api/orders/{orderId}/serve", orderId)
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .content(content))
+                    // then
+                    .andExpect(status().isOk());
+        }
+    }
 }
