f0bd21cbe43c0195cbe583c8f1e02e8673e777ab
ALbertIM0427 <guang7226961@naver.com>
2025-02-08 23:57:18 +0900

    test(application): 포장 주문 완료 테스트 구현

--- src/test/java/kitchenpos/application/OrderServiceTest.java
+++ src/test/java/kitchenpos/application/OrderServiceTest.java
@@ -125,6 +125,16 @@ class OrderServiceTest {
     private static @NotNull Order createFixOrder(
             UUID orderId,
             OrderStatus orderStatus,
+            OrderType orderType
+    ) {
+        var order = createFixOrder(orderId, orderStatus);
+        order.setType(orderType);
+        return order;
+    }
+
+    private static @NotNull Order createFixOrder(
+            UUID orderId,
+            OrderStatus orderStatus,
             OrderType orderType,
             String deliveryAddress,
             List<OrderLineItem> orderLineItems
@@ -962,6 +972,69 @@ class OrderServiceTest {
                 // then
                 assertThat(servedOrder).isNotNull();
                 assertThat(servedOrder.getStatus()).isEqualTo(OrderStatus.SERVED);
+            }
+        }
+
+        @DisplayName("주문 완료")
+        @Nested
+        class Complete {
+
+            @DisplayName("주문 아이디가 null인 경우 예외를 던진다.")
+            @Test
+            void if_order_id_is_null_then_throw_exception() {
+                // when, then
+                assertThatThrownBy(() -> orderService.complete(null))
+                        .isInstanceOf(NoSuchElementException.class);
+            }
+
+            @DisplayName("주문이 존재하지 않는 경우 예외를 던진다.")
+            @Test
+            void if_order_does_not_exist_then_throw_exception() {
+                // given
+                var orderId = UUID.randomUUID();
+
+                given(orderRepository.findById(orderId))
+                        .willReturn(Optional.empty());
+
+                // when, then
+                assertThatThrownBy(() -> orderService.complete(orderId))
+                        .isInstanceOf(NoSuchElementException.class);
+            }
+
+            @DisplayName("주문 상태가 서빙 상태가 아닌 경우 예외를 던진다.")
+            @ParameterizedTest
+            @ValueSource(strings = {"WAITING", "ACCEPTED", "DELIVERING", "DELIVERED", "COMPLETED"})
+            void if_order_status_is_not_served_then_throw_exception(OrderStatus orderStatus) {
+                // given
+                var orderId = UUID.randomUUID();
+
+                var order = createFixOrder(orderId, orderStatus, OrderType.TAKEOUT);
+
+                given(orderRepository.findById(orderId))
+                        .willReturn(Optional.of(order));
+
+                // when, then
+                assertThatThrownBy(() -> orderService.complete(orderId))
+                        .isInstanceOf(IllegalStateException.class);
+            }
+
+            @DisplayName("주문 완료 성공하면 주문 상태를 완료로 변경한다.")
+            @Test
+            void if_success_then_change_order_status_to_completed() {
+                // given
+                var orderId = UUID.randomUUID();
+
+                var order = createFixOrder(orderId, OrderStatus.SERVED);
+
+                given(orderRepository.findById(orderId))
+                        .willReturn(Optional.of(order));
+
+                // when
+                var completedOrder = orderService.complete(orderId);
+
+                // then
+                assertThat(completedOrder).isNotNull();
+                assertThat(completedOrder.getStatus()).isEqualTo(OrderStatus.COMPLETED);
             }
         }
     }
